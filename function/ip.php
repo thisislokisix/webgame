bac7ee5b533ec835689e14a29bef9511b6c2b612
Lokisix <lokistock444@gmail.com>
2025-01-12 12:26:06 +0700

    Create ip.php

---
+++ function/ip.php
@@ -0,0 +1,15 @@
+<?php
+function getUserIp()
+{
+    $ip = null;
+
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ip;
+}
